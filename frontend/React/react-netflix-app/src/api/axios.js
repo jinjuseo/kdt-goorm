d3b9b626f17dff1303779b248d00dd350d1ad4e3
Jinju Seo <wjbbdq1223@naver.com>
2024-03-13 18:17:54 +0900

    feat(app)

--- frontend/React/react-netflix-app/src/api/axios.js
+++ frontend/React/react-netflix-app/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https:/api.themoviedb.org/3",
   params: {
-    api_key: "018d7ccf8d538d4bfa744881cc7d8d79",
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
     language: "ko-KR",
   },
 });
